0d7df3f4ab8bf4e026447f523e83e493aa74334b
unknown <rislammb@gmail.com>
2023-12-28 09:23:28 +0600

    complete chapter 12 with create, update and delete invoice server actions

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,32 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import EditInvoiceForm from '@/app/ui/invoices/edit-form';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: 'Invoices',
+            href: '/dashboard/invoices',
+          },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+}
