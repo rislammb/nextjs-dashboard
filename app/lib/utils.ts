3360617fb113da184b969426f6adfa254e9ccf5a
unknown <rislammb@gmail.com>
2024-01-07 10:41:50 +0600

    update utility and ui for dashboard revenue

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -21,15 +21,33 @@ export const formatDateToLocal = (
   return formatter.format(date);
 };
 
+export const formatMonthToLocal = (
+  dateStr: string,
+  locale: string = 'en-US',
+) => {
+  const date = new Date(dateStr);
+  const options: Intl.DateTimeFormatOptions = {
+    month: 'short',
+    year: 'numeric',
+  };
+  const formatter = new Intl.DateTimeFormat(locale, options);
+  return formatter.format(date);
+};
+
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
   const yAxisLabels = [];
   const highestRecord = Math.max(...revenue.map((month) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
+  const devidedBy = highestRecord / 10;
 
-  for (let i = topLabel; i >= 0; i -= 1000) {
-    yAxisLabels.push(`$${i / 1000}K`);
+  for (let i = topLabel; i >= 0; i -= devidedBy) {
+    if (topLabel / 100 / 1000 < 10) {
+      yAxisLabels.push(`$${(i / 100 / 1000).toFixed(1)}K`);
+    } else {
+      yAxisLabels.push(`$${Math.floor(i / 100 / 1000)}K`);
+    }
   }
 
   return { yAxisLabels, topLabel };
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,4 +1,5 @@
 import { fetchRevenue } from '@/app/lib/data';
+import { Revenue } from '@/app/lib/definitions';
 import { generateYAxis } from '@/app/lib/utils';
 import { lusitana } from '@/app/ui/fonts';
 import { CalendarIcon } from '@heroicons/react/24/outline';
@@ -29,7 +30,7 @@ export default async function RevenueChart() {
       {/* NOTE: comment in this code when you get to this point in the course */}
 
       <div className="rounded-xl bg-gray-50 p-4">
-        <div className="mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4">
+        <div className="mt-0 flex gap-2 rounded-md bg-white p-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
@@ -39,19 +40,24 @@ export default async function RevenueChart() {
             ))}
           </div>
 
-          {revenue.map((month) => (
-            <div key={month.month} className="flex flex-col items-center gap-2">
+          <div className=" grid grid-cols-12 items-end gap-2  sm:grid-cols-12 md:gap-4">
+            {revenue.map((month: Revenue) => (
               <div
-                className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
-                }}
-              ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
-                {month.month}
-              </p>
-            </div>
-          ))}
+                key={month.month}
+                className="flex flex-col items-center gap-2"
+              >
+                <div
+                  className="w-full rounded-md bg-blue-300"
+                  style={{
+                    height: `${(chartHeight / topLabel) * month.revenue}px`,
+                  }}
+                ></div>
+                <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
+                  {month.month.split(' ')[0]}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
